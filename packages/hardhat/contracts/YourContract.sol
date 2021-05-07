92b556d1e0957c6ef9cd093d10be4f045096fec5
Brian Cottrell <Brian.Cottrell.mobile@gmail.com>
2021-05-07 10:51:29 -0700

    Update YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,24 +1,36 @@
 pragma solidity >=0.6.0 <0.9.0;
-//SPDX-License-Identifier: MIT
+pragma experimental ABIEncoderV2;
 
 import "hardhat/console.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+// import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract YourContract {
-
-  event SetPurpose(address sender, string purpose);
-
-  string public purpose = "I updated this purpose.";
+  uint256 public price = 0;
+  uint256 public maxResolutionTime = 0;
+  string[] public supportedTokens;
   address public owner = 0xe5050eC33578Cb72554B696e6721369B660F3C72;
 
-  constructor() {
-    // what should we do on deploy?
+  event SetPrice(address sender, uint256 price);
+  event SetMaxResolutionTime(address sender, uint256 maxResolutionTime);
+  event SetSupportedTokens(address sender, string[] supportedTokens);
+
+  constructor() public {
   }
 
-  function setPurpose(string memory newPurpose) public {
-    purpose = newPurpose;
-    console.log(msg.sender,"set purpose to",purpose);
-    emit SetPurpose(msg.sender, purpose);
+  function setPrice(uint256 _price) public {
+    price = _price;
+    emit SetPrice(msg.sender, price);
+  }
+
+  function setMaxResolutionTime(uint256 _maxResolutionTime) public {
+    maxResolutionTime = _maxResolutionTime;
+    emit SetMaxResolutionTime(msg.sender, maxResolutionTime);
+  }
+
+  function setSupportedTokens(string[] memory _supportedTokens) public {
+    supportedTokens = _supportedTokens;
+    emit SetSupportedTokens(msg.sender, supportedTokens);
   }
 
 }
